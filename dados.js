7e7df031ff9e70a78e412c523abff069f553aeeb
hendreus007 <61298404+hendreus007@users.noreply.github.com>
2024-09-07 20:25:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    
+  {
+    titulo: "Naruto Uzumaki",
+    descricao: "Um ninja que sonha em se tornar Hokage da Vila da Folha e ser reconhecido por todos.",
+    caracteristicas: "Determinado, persistente, otimista, leal a seus amigos.",
+    link: "https://pt.wikipedia.org/wiki/Naruto_Uzumaki",
+    tags: "Otimista hokage ninja"
+  },
+  {
+    titulo: "Goku",
+    descricao: "Um sayajin com o objetivo de se tornar o guerreiro mais forte do universo.",
+    caracteristicas: "Ingênuo, otimista, batalhador, protetor de seus amigos.",
+    link: "https://pt.wikipedia.org/wiki/Goku",
+    tags: "guerreiro lutador poderoso"
+  },
+  {
+    titulo: "Monkey D. Luffy",
+    descricao: "Um pirata que busca o One Piece e se tornar o Rei dos Piratas.",
+    caracteristicas: "Aventureiro, otimista, teimoso, leal à sua tripulação.",
+    link: "https://pt.wikipedia.org/wiki/Monkey_D._Luffy",
+    tags: "barco sonhador amigo"
+  }
+];
